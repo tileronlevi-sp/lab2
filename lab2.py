8690197db6dd1156d5b18ca50efa7dc6cda8e434
levi <tileronlevi.22@ichat.sp.edu.sg>
2022-04-25 10:35:39 +0800

    version 2

--- lab2.py
+++ lab2.py
@@ -1,1 +1,21 @@
-print("HELLO WORLD")
+def user_input():
+    print("hello")
+    list = []
+    for i in range(5):
+        num = input("Type a number: ")
+        list.append(int(num))
+    print(list)
+    return list
+
+def cal_avg(avg):
+    all = sum(avg)
+    print(len(avg))
+    average = all/len(avg)
+    print("Average is ", average)
+    print("Min is ", min(avg))
+    print("Max is ", max(avg))
+
+
+if __name__ == '__main__':
+    num_given = user_input()
+    cal_avg(num_given)
